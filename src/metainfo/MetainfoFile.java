b152bf5b229a25d198c55fb09df9d4f0f41553d4
Hubert <hubert15nowak@gmail.com>
2020-06-01 17:24:45 +0200

    add MetainfoFile

---
+++ src/metainfo/MetainfoFile.java
@@ -0,0 +1,13 @@
+package metainfo;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class MetainfoFile {
+    String announce;
+    String announceList;
+    String comment;
+    String createdBy;
+    String creationDate;
+    Dictionary info;
+}
