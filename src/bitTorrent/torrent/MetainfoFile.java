639c700b30ae4288cbee603c5c528f5133be4d45
Hubert <hubert15nowak@gmail.com>
2020-06-08 22:54:25 +0200

    fix

--- src/bitTorrent/torrent/MetainfoFile.java
+++ src/bitTorrent/torrent/MetainfoFile.java
@@ -5,8 +5,7 @@ import bitTorrent.peer.local.MyFile;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Hashtable;
+import java.util.HashMap;
 
 public class MetainfoFile {
     String announce;
@@ -14,42 +13,45 @@ public class MetainfoFile {
     String comment;
     String createdBy;
     String creationDate;
-    Dictionary info;
+    HashMap<String, Object> info;
 
     private MetainfoFile(String announce, String announceList, String comment, String createdBy) {
         this.announce = announce;
         this.announceList = announceList;
         this.comment = comment;
         this.createdBy = createdBy;
-        info = new Hashtable();
+        info = new HashMap<>();
     }
 
-    static MetainfoFile createTorrent(String announce, String announceList, String comment, String createdBy, MyFile file, int pieceSizeKiB) throws NoSuchAlgorithmException {
+    public static MetainfoFile createTorrent(String announce, String announceList, String comment, String createdBy, MyFile file, int pieceSizeKiB) throws NoSuchAlgorithmException {
         MetainfoFile metainfoFile = new MetainfoFile(announce, announceList, comment, createdBy);
         metainfoFile.info.put("length", file.getContent().length);
         //metainfoFile.info.put("md5sum", file.getMd5Sum()); TODO: create md5
         metainfoFile.info.put("name", file.getName());
-        metainfoFile.info.put("piece length", pieceSizeKiB);
+        metainfoFile.info.put("piece length", pieceSizeKiB*1000);
         metainfoFile.info.put("pieces", TorrentFile.getPiecesSha(file, pieceSizeKiB));
 
         return metainfoFile;
     }
 
-    static MetainfoFile createTorrent(String announce, String announceList, String comment, String createdBy, Directory dir, int pieceSizeKiB) throws NoSuchAlgorithmException {
+    public static MetainfoFile createTorrent(String announce, String announceList, String comment, String createdBy, Directory dir, int pieceSizeKiB) throws NoSuchAlgorithmException {
         MetainfoFile metainfoFile = new MetainfoFile(announce, announceList, comment, createdBy);
-        ArrayList<Directory> directories = dir.getDirectories();
         ArrayList<TorrentFile> allFiles = new ArrayList<>();
+        ArrayList<HashMap<String, Object>> files= new ArrayList<>();
 
         ArrayList<String> seperatePieces = new ArrayList<>();
         //Pojedyncze pliki w folderze
         iterDir(allFiles, dir, new StringBuilder(dir.getName()));
         for(TorrentFile f: allFiles){
-            metainfoFile.info.put("length", f.file.getContent().length);
+            HashMap<String, Object> dict = new HashMap<>();
+            dict.put("length", f.file.getContent().length);
             //metainfoFile.info.put("md5sum", f.getMd5Sum()); TODO: create md5
-            metainfoFile.info.put("name", f.path + "/" + f.file.getName());
-            metainfoFile.info.put("piece length", pieceSizeKiB);
+            dict.put("name", f.path + "/" + f.file.getName());
+            dict.put("piece length", pieceSizeKiB);
+            files.add(dict);
             seperatePieces.add(TorrentFile.getPiecesSha(f.file, pieceSizeKiB));
         }
+        metainfoFile.info.put("files", files);
         StringBuilder pieces = new StringBuilder();
         seperatePieces.forEach(pieces::append);
         metainfoFile.info.put("pieces", pieces.toString());
@@ -65,4 +67,10 @@ public class MetainfoFile {
             iterDir(files, d, path.append("/").append(d.getName()));
         }
     }
+
+    public HashMap<String, Object> getInfo() {
+        return info;
+    }
+
+
 }
