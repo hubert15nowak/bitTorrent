f73170ab35a4da85e8a45e7cf218b1be0f3226c7
Hubert <hubert15nowak@gmail.com>
2020-06-10 19:43:59 +0200

    create Peer class

---
+++ src/bitTorrent/peer/Peer.java
@@ -0,0 +1,73 @@
+package bitTorrent.peer;
+
+import bitTorrent.network.*;
+import bitTorrent.peer.local.Disk;
+import bitTorrent.peer.pwp.PWPClient;
+import bitTorrent.peer.torrent.Torrent;
+import flow.NextAction;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Peer extends NetworkClient implements NextAction {
+    private int fixedBlockSizeB = 256;
+    private UUID id;
+    private Disk disk = Disk.randomDisk();
+
+    private ArrayList<Torrent> torrents = new ArrayList<>();
+    private ArrayList<PWPClient> newPWP = new ArrayList<>();
+
+    public Peer(String address, int port, Network network) {
+        super(address, port, network);
+
+    }
+
+
+    @Override
+    public void nextStep() {
+        super.nextStep();
+        for (Torrent torrent : torrents) {
+            torrent.nextStep();
+        }
+    }
+
+    @Override
+    protected void receiveSocket(NetworkSocket socket) {
+        PWPClient pwpClient = new PWPClient(id, socket);
+        socket.setHandler(pwpClient);
+        newPWP.add(pwpClient);
+    }
+
+
+    public int getFixedBlockSizeB() {
+        return fixedBlockSizeB;
+    }
+
+    public void setFixedBlockSizeB(int fixedBlockSizeB) {
+        this.fixedBlockSizeB = fixedBlockSizeB;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public ArrayList<Torrent> getTorrents() {
+        return torrents;
+    }
+
+    public void setTorrents(ArrayList<Torrent> torrents) {
+        this.torrents = torrents;
+    }
+
+    public ArrayList<PWPClient> getNewPWP() {
+        return newPWP;
+    }
+
+    public void setNewPWP(ArrayList<PWPClient> newPWP) {
+        this.newPWP = newPWP;
+    }
+}
