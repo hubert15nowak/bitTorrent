71e6799e1417bc066d5d6a883000b9196019b2fb
Hubert <hubert15nowak@gmail.com>
2020-06-10 19:48:19 +0200

    add singleton and addresses disposal

--- src/bitTorrent/network/Network.java
+++ src/bitTorrent/network/Network.java
@@ -7,17 +7,40 @@ import java.util.ArrayList;
 public class Network {
     static ArrayList<NetworkClient> clients = new ArrayList<>();
 
+    static Network network = new Network();
+
+
+    private Network(){}
+
+    public static Network getInstance() {
+        return network;
+    }
+
 
     public static void receive(Message message) throws Exception{
         for (NetworkClient client : clients) {
-            if (client.addres.equals(message.destinationAdrress)) {
-                client.receive(message);
+            if (client.address.equals(message.destinationAddress)) {
+                client.receiveMessage(message);
+                break;
             }
         }
     }
 
 
+    public void add(NetworkClient client) {
+        clients.add(client);
+    }
 
 
+
+    private static int address1 = 0;
+    private static int address2 = 1;
+    public static String getNewAddress() {
+        if(address2 == 255) {
+            address1++;
+            address2 = 0;
+        }
+        return "283.142."+address1+"."+address2++;
+    }
 
 }
