536c65ab613f440911384e0b40629806cb388fa4
Hubert <hubert15nowak@gmail.com>
2020-06-10 19:54:21 +0200

    add NetworkClient and torrent info list

--- src/bitTorrent/tracker/Tracker.java
+++ src/bitTorrent/tracker/Tracker.java
@@ -1,5 +1,29 @@
 package bitTorrent.tracker;
 
-public class Tracker {
-    
+import bitTorrent.network.Network;
+import bitTorrent.network.NetworkClient;
+import bitTorrent.network.NetworkSocket;
+
+import java.util.ArrayList;
+
+public class Tracker extends NetworkClient {
+
+    private int interval = 5000;
+
+    ArrayList<TorrentInfo> torrents;
+
+    public Tracker(String address, int port, Network network) {
+        super(address, port, network);
+    }
+
+    @Override
+    protected void receiveSocket(NetworkSocket socket) {
+
+    }
+
+    @Override
+    public void nextStep() {
+        super.nextStep();
+
+    }
 }
